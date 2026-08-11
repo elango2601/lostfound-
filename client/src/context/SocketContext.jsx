1983464a29c090824c936fb5ac4b0ff37f4197e4
elango2601 <elangopugal26@gmail.com>
2026-08-12 05:05:43 +0530

    Deploy: Support production env variables for Vercel + Render deployment

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -11,7 +11,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (user) {
-      const backendUrl = `http://${window.location.hostname}:5001`;
+      const backendUrl = import.meta.env.VITE_SOCKET_URL || `http://${window.location.hostname}:5001`;
       
       const newSocket = io(backendUrl, {
         reconnection: true,
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api', // Will be proxied by Vite
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   withCredentials: true
 });
 
